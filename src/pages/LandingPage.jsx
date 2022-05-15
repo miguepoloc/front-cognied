c3b75a5a6fccc03bbe80b7484bf923d08b2a8195
Daniel Vega <42049035+dan077@users.noreply.github.com>
2022-05-15 02:48:53 -0500

    Vocabulario casi terminado y ruleda adelantadas

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -12,6 +12,7 @@ import Scroll from '../helpers/helperScroll'
 import Surveys from '../components/Surveys/Surveys'
 import Part2 from '../components/Act_1.1/Part2'
 import { Vocabulario } from '../components/Act_2.2/Vocabulario'
+import RuletaEmociones from '../components/Act_2.2/RuletaEmociones'
 function LandingPage () {
   // El id servirá para nombrar los identificadores y el txt para colocar el nombre (por ejemplo en la barra de navegacion.)
   // Adicional a eso permitirá hacer pocas modificaciones en el codigo
@@ -42,7 +43,8 @@ function LandingPage () {
       <ButtonToUp Scroll={scroll} seccionToUp={secciones.Home.id} />
       {/* <Surveys /> */}
         {/* <Part2 />   */}
-       <Vocabulario/> 
+       {/* <Vocabulario/>  */}
+       <RuletaEmociones/>
     </>
   )
 }
