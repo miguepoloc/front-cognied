f15f44d98bfbb441329100070a948e0fb0a09100
miguepoloc <miguepoloc@gmail.com>
2022-05-16 09:35:59 -0500

    Pruebas de Put data

--- src/components/Dashboard/ItemCapsula.jsx
+++ src/components/Dashboard/ItemCapsula.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Button } from 'react-bootstrap'
 import { useHistory } from 'react-router-dom'
 
--- src/pages/ModuloEmocionalCapsula.jsx
+++ src/pages/ModuloEmocionalCapsula.jsx
@@ -9,9 +9,46 @@ import NavBarDashboard from '../components/Dashboard/NavBarDashboard'
 import FooterDashboard from '../components/Dashboard/FooterDashboard'
 import ButtonLibro from '../components/Dashboard/ButtonLibro'
 
-import { useLocation } from 'react-router-dom'
+import { useHistory, useLocation } from 'react-router-dom'
 import RuletaEmociones from '../components/Act_2.2/RuletaEmociones'
 import { Button } from 'react-bootstrap'
+
+async function putData () {
+  const url = 'http://127.0.0.1:8002/api/seccion_emocional/1/'
+  const data = {
+    id_seccion: 1,
+    usuario: 'prueba',
+    capsula1: false,
+    capsula2: false,
+    actividad1: true,
+    capsula3: true,
+    actividad2: true,
+    capsula4: true,
+    actividad3: false,
+    capsula5: true,
+    capsula6: true,
+    actividad4: true,
+    capsula7: true,
+    capsula8: true
+  }
+
+  await fetch(url, {
+    method: 'PUT', // or 'PUT'
+    body: JSON.stringify(data), // data can be `string` or {object}!
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+    .then((datos) => datos.json())
+    .then((datos) => {
+      console.log(datos)
+      return datos
+    })
+    .catch((err) => {
+      console.log(err)
+      return null
+    })
+}
 
 const ModuloEmocionalCapsula = ({ capsula, nombrecapsula }) => {
   // assigning location variable
@@ -22,6 +59,50 @@ const ModuloEmocionalCapsula = ({ capsula, nombrecapsula }) => {
 
   // Javascript split method to get the name of the path in array
   const splitLocation = pathname.split('/')
+
+  const history = useHistory()
+
+  function cambioBoton () {
+    switch (splitLocation[1]) {
+      case 'emocionalcap1':
+        putData()
+        history.push('/emocionalcap2')
+        break
+      case 'emocionalcap2':
+        history.push('/emocionalejer1')
+        break
+      case 'emocionalejer1':
+        history.push('/emocionalcap3')
+        break
+      case 'emocionalcap3':
+        history.push('/emocionalejer2')
+        break
+      case 'emocionalejer2':
+        history.push('/emocionalcap4')
+        break
+      case 'emocionalcap4':
+        history.push('/emocionalejer3')
+        break
+      case 'emocionalejer3':
+        history.push('/emocionalcap5')
+        break
+      case 'emocionalcap5':
+        history.push('/emocionalcap6')
+        break
+      case 'emocionalcap6':
+        history.push('/emocionalejer4')
+        break
+      case 'emocionalejer4':
+        history.push('/emocionalcap7')
+        break
+      case 'emocionalcap7':
+        history.push('/emocionalcap8')
+        break
+      case 'emocionalcap8':
+        history.push('/dashboard')
+        break
+    }
+  }
   return (
         <>
             <div className="g-sidenav-show  bg-gray-100">
@@ -30,7 +111,7 @@ const ModuloEmocionalCapsula = ({ capsula, nombrecapsula }) => {
                 <main className="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
                     <NavBarDashboard section={nombrecapsula} />
                     <div className="container-fluid py-4">
-                        <div >
+                        <div>
                             {splitLocation[1].slice(0, 12) === 'emocionalcap'
                               ? <img src={capsula} alt="capsula" className='img-capsula' />
                               : <RuletaEmociones />
@@ -38,9 +119,16 @@ const ModuloEmocionalCapsula = ({ capsula, nombrecapsula }) => {
                             }
 
                         </div>
-                        <Button variant="primary" size="lg" disabled>
-                            Primary button
+                        <hr />
+                        <Button
+                            className='botoncentrado'
+                            variant="success"
+                            size="lg"
+                            onClick={cambioBoton}
+                        >
+                            Siguiente
                         </Button>
+
                         <FooterDashboard />
                         <ButtonLibro />
 
